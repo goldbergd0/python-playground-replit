4fa799f67364f653901c8461668917e6a4d6689f
Daniel Goldberg <dgoldberg92@gmail.com>
2021-01-05 14:46:54 +0000

    more sql tests

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sqlite3
+import random
 
-c = sqlite3.connect('test.db').cursor()
+c = sqlite3.connect('test.db', isolation_level=None).cursor()
 
 # c.execute('create table test("# test","# another")')
-c.execute('insert into test("# test", "# another") values (?, ?)', (1,2,))
-c.execute("select rowid,* from test")
-for r in c.fetchall():
+c.execute('insert into test("# test", "# another") values (?, ?)', (1,random.randint(1,10),))
+c.execute("select * from test where \"# another\" > 2 order by \"# another\" ASC;")
+r = c.fetchone()
+while r:
   print(r)
+  r = c.fetchone()
