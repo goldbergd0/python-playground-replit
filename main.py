164671a882275b1be24642118139ec80335540ae
Daniel Goldberg <dgoldberg92@gmail.com>
2021-01-07 18:59:28 +0000

    Added an insert string creator

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import sqlite3
 import random
 
+
+
+def create_insert_string(table_name, columns):
+  names = '('
+  names += ','.join(columns)
+  names += ')'
+  vals = '('
+  vals += ','.join('?' for c in columns)
+  vals += ')'
+  return 'INSERT INTO {}{} VALUES {}'.format(table_name, names, vals)
+
+
 c = sqlite3.connect('test.db', isolation_level=None).cursor()
 
-# c.execute('create table test("# test","# another")')
+c.execute('create table test2(test,another)')
 c.execute('insert into test("# test", "# another") values (?, ?)', (1,random.randint(1,10),))
-c.execute("select * from test where \"# another\" > 2 order by \"# another\" ASC;")
+c.execute('select MAX(\"# another\") from test2 where \"# another\" > 2 order by \"# another\" ASC;')
 r = c.fetchone()
 while r:
-  print(r)
+  print(r[0])
   r = c.fetchone()
+
+print(create_insert_string('TEST_NAME', ['col1', 'col2', 'col3']))
