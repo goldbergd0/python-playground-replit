cc9768ac26de5fb9b2da499585876e2daf637c30
Daniel Goldberg <dgoldberg92@gmail.com>
2021-01-11 22:09:42 +0000

    Get tables and columns

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ def create_insert_string(table_name, columns):
   vals = '('
   vals += ','.join('?' for c in columns)
   vals += ')'
-  return 'INSERT INTO {}{} VALUES {}'.format(table_name, names, vals)
+  return 'INSERT INTO {}{} VALUES {};'.format(table_name, names, vals)
 
 
 c = sqlite3.connect('test.db', isolation_level=None).cursor()
 
-c.execute('create table test2(test,another)')
-c.execute('insert into test("# test", "# another") values (?, ?)', (1,random.randint(1,10),))
-c.execute('select MAX(\"# another\") from test2 where \"# another\" > 2 order by \"# another\" ASC;')
+# c.execute('create table test2(test,another)')
+c.execute('insert into test("# test", "# another") values (?, ?);', [1,random.randint(1,10),])
+c.execute('select rowid,* from test;')
 r = c.fetchone()
 while r:
-  print(r[0])
+  print(r)
   r = c.fetchone()
+c.execute('SELECT name FROM sqlite_master WHERE type="table";')
+print(c.fetchall())
+c.execute('SELECT * FROM test LIMIT 1;')
+for d in c.description:
+  print(d[0])
 
-print(create_insert_string('TEST_NAME', ['col1', 'col2', 'col3']))
+print(create_insert_string('TEST_NAME', ['col1', 'col2', 'col3', 'COL4'.lower()]))
